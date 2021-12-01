f1cf442257b66fc071cb8801d1e99e3c1455656e
chasepg0821 <91268417+chasepg0821@users.noreply.github.com>
2021-12-01 18:49:10 -0500

    Draw and Redraw
    Added the mechanics to get a random animal and redraw if the animal is not favorable to the players.

--- charades_game.py
+++ charades_game.py
@@ -28,20 +28,26 @@ print("Number of points needed to win the game: " + pointsToWin)
 
 while not isWinner:
 
-    print("\n\nPlayer " + str(currentPlayer) + ", your word is: (from api)") # MAYBE ADD TIME LIMIT
+    response = requests.get("https://random-word-form.herokuapp.com/random/animal")
+    animal = response.json()[0]
+
+    print("\n\nPlayer " + str(currentPlayer) + ", your word is: " + animal + "\n\n") # MAYBE ADD TIME LIMIT
 
     # REQUEST RANDOM ANIMAL FROM API
 
     while wrongWinner:
 
-        currentWinner = input("\nType the player's # who won: ")
+        currentWinner = input("\nType the player's # who won (or 0 to skip animal and draw again): ")
         currentWinner = int(currentWinner)
 
         if currentWinner > 0 & currentWinner <= numPlayers:
             wrongWinner = False
+        elif currentWinner == 0:
+            print("Redrawing Animal...")
+            wrongWinner = False
         else:
             print("Not a player number! Try again!")
-
+    wrongWinner = True
    
     if currentWinner == 1:
         scores[0] += 1
@@ -62,8 +68,8 @@ while not isWinner:
             for i in range(numPlayers):
                 print("Player " + str(i + 1) + ": " + str(scores[i]))
 
-
-    if currentPlayer < numPlayers:
-        currentPlayer += 1
-    else:
-        currentPlayer = 1
+    if currentWinner != 0:
+        if currentPlayer < numPlayers:
+            currentPlayer += 1
+        else:
+            currentPlayer = 1
